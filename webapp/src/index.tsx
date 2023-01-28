e2df571863afae8ab5406cb6f70ea0812610d598
Reyis Jones <reyisj@gmail.com>
2023-01-28 19:57:57 -0400

    Adding lint and package.json scripts

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import './assets/index.scss';
 
-
-const Application: React.SFC<{}> = () => (
-  <h1>Welcome</h1>
+const Application: React.FC<unknown> = () => (
+  <><h1>Welcome</h1><div>Template using React with Typescript</div></>
 );
 
 render(<Application />, document.getElementById('root'));
